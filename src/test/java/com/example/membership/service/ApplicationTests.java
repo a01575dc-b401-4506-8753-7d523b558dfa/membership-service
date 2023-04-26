54f2b1711e5322f8743279964262524fe62fa91a
a01575dc-b401-4506-8753-7d523b558dfa <a01575dc-b401-4506-8753-7d523b558dfa@riseup.net>
2023-04-26 21:38:02 +0900

    Add empty project

---
+++ src/test/java/com/example/membership/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membership.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
