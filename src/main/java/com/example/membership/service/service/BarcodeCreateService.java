ab45dde68ccda7e8ea6b1456433e47b6d404d5ac
dmdwns2 <ssreki1@naver.com>
2023-06-17 16:22:05 +0900

    Revert "- 유저ID로 바코드 ID가 있는지 확인 후 있으면 원래 있던 바코드 ID를 반환하는 코드 추가"
    This reverts commit e1ebb1b0a1655836f5d22338de14aa2ea8a94d55.

--- src/main/java/com/example/membership/service/service/BarcodeCreateService.java
+++ src/main/java/com/example/membership/service/service/BarcodeCreateService.java
@@ -1,6 +1,9 @@
 package com.example.membership.service.service;
 
-import com.example.membership.service.infra.entity.*;
+import com.example.membership.service.infra.entity.Membership;
+import com.example.membership.service.infra.entity.MembershipRepository;
+import com.example.membership.service.infra.entity.User;
+import com.example.membership.service.infra.entity.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -14,21 +17,15 @@ public class BarcodeCreateService {
     private final UserRepository userRepository;
     private final MembershipRepository membershipRepository;
 
-    public Membership createBarcode(Long userId) {
-        final User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException(userId));
-        return membershipRepository.findByUserId(user.getId())
-                .orElseGet(() -> saveAndGetBarcode(user));
-    }
+    public void createBarcode(Long userId) {
+        final User user = userRepository.findById(userId).orElseThrow(() -> {
+            throw new RuntimeException("id없음");
+        });
 
-    private Membership saveAndGetBarcode(User user) {
         String uuid = RandomStringUtils.randomNumeric(10);
-        return membershipRepository.save(
-                Membership.builder()
-                        .id(uuid)
-                        .user(user)
-                        .build()
-        );
+        Membership barcode = Membership.builder().id(uuid).user(user).build();
+
+        membershipRepository.save(barcode);
     }
 }
 
