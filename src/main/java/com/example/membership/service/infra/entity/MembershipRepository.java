3346c04e8394e6b31669c3f2f9b5a5d8edf10bd1
dmdwns2 <ssreki1@naver.com>
2023-06-17 16:18:28 +0900

    findByuserId 메서드 추가

--- src/main/java/com/example/membership/service/infra/entity/MembershipRepository.java
+++ src/main/java/com/example/membership/service/infra/entity/MembershipRepository.java
@@ -2,5 +2,8 @@ package com.example.membership.service.infra.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MembershipRepository  extends JpaRepository<Membership, String> {
+    Optional<Membership> findByUserId(Long userId);
 }
