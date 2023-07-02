24fb72fdfb76aec06cf826110bdda9a0e05a7512
dmdwns2 <ssreki1@naver.com>
2023-07-02 16:18:39 +0900

    - service에 트랜잭션 삭제 - BarcodeSaveAndGetException 에러 설명을 명확하게 변경

--- src/main/java/com/example/membership/service/infra/entity/BarcodeSaveAndGetException.java
+++ src/main/java/com/example/membership/service/infra/entity/BarcodeSaveAndGetException.java
@@ -2,6 +2,6 @@ package com.example.membership.service.infra.entity;
 
 public class BarcodeSaveAndGetException extends RuntimeException {
     public BarcodeSaveAndGetException(Long userId) {
-        super("problem generating barcode [id "+ userId + "]");
+        super("Duplicate barcode generation requested [id "+ userId + "]");
     }
 }
--- src/main/java/com/example/membership/service/service/BarcodeCreateService.java
+++ src/main/java/com/example/membership/service/service/BarcodeCreateService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class BarcodeCreateService {
 
     private final UserRepository userRepository;
